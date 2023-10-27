eed34687f8de41bcdbee29a734e4922dab89d685
Mauro Felipe <mfelipxl@gmail.com>
2023-10-26 21:51:00 -0400

    Initial commit

---
+++ public/assets/js/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+a;
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
